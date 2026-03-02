94de42a95bd5175a11dcb4223dd676b91779e9e8
ryanghosling56-lgtm <ryanghosling56@gmail.com>
2026-03-02 14:51:59 +0700

    lanjutannn

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,5 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-//Route::get('/user', function (Request $request) {
-   // return $request->user();
-//})->middleware('auth:sanctum');
-
 Route::get('/cekdatabase', [App\Http\Controllers\cekcontroller::class, 'index']);
-
 
